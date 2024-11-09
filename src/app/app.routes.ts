c5acd95bd294b35fccf694deafff6795b0bcc871
Well Lins <wellingtonlinsdev@outlook.com>
2024-11-09 15:27:32 -0300

    Fix: Add routes summoner

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 
 // Component Pages
 import { HomeComponent } from './features/home/home.component';
+import { SummonerComponent } from './features/summoner/summoner.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent,
-    }
+    {path: '',component: HomeComponent},
+
+    {path: 'summoner/:nickname/:riotId',component: SummonerComponent}
 ];
