6c36b17683f1a6c2a31c27e7783c73a799921958
Well Lins <wellingtonlinsdev@outlook.com>
2024-11-08 22:46:36 -0300

    Fix: Create summoner page

---
+++ src/app/features/summoner/summoner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-summoner',
+  standalone: true,
+  imports: [],
+  templateUrl: './summoner.component.html',
+  styleUrl: './summoner.component.scss'
+})
+export class SummonerComponent {
+
+}
